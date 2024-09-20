fb40256f1af72ba855420994db6fe3762d41ec14
Charles Shea <chashea@microsoft.com>
2024-09-20 12:14:21 -0400

    update main

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -18,7 +18,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "/subscriptions/${data.azurerm_client_config.this.subscription_id}"
+  subscription_id = "xxxxx-xxxx-xxxx-xxxx-xxxxx"
 }
 
 
