b1a437845b25bdf3de97fecbd5e49a12dc20db0d
Charles Shea <chashea@microsoft.com>
2024-09-25 10:11:37 -0400

    fix provider registration

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -18,7 +18,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "xxxxx-xxxx-xxxx-xxxx-xxxxx"
+  subscription_id = "0ec38a31-9a35-44fe-ac59-8347ac564cef"
 }
 
 
@@ -53,16 +53,12 @@ locals {
 data "azurerm_client_config" "this" {}
 
 resource "azapi_resource_action" "resource_provider_registration" {
-  for_each = local.resource_providers_to_register
-
   resource_id = "/subscriptions/${data.azurerm_client_config.this.subscription_id}"
   type        = "Microsoft.Resources/subscriptions@2021-04-01"
-  action      = "providers/${each.value.resource_provider}/register"
+  action      = "providers/${local.resource_providers_to_register.dev_center.resource_provider}/register"
   method      = "POST"
 }
 
-
-# This is required for resource modules
 resource "azurerm_resource_group" "this" {
   location = module.regions.regions[random_integer.region_index.result].name
   name     = module.naming.resource_group.name_unique
